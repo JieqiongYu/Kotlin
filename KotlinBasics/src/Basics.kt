db5060427e9ca96899cacb5999bbf77f7a6e613a
Mer <yjieqiong@gmail.com>
2018-01-07 01:05:36 +0900

    Integer and String

--- KotlinBasics/src/Basics.kt
+++ KotlinBasics/src/Basics.kt
@@ -8,4 +8,14 @@ fun main(args: Array<String>) {
     println(age)
     println(candy)
 
+    // Integer: specify a variable as integer
+    var ageSpec : Int = 29
+    // String: specify a variable as String
+    var candySpec : String = "Snickers"
+
+    // Without specify is better than specifying one
+    var wallet = 70
+    println(wallet)
+
+
 }
